88e29101d55db4c227a83285cdeaba1a1eaa5467
Dan Freeman <dfreeman@salsify.com>
2016-03-06 16:27:19 -0500

    Modules + @import proof of concept

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    cssModules: {
+      plugins: [
+        require('postcss-import'),
+        require('postcss-css-variables')
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
